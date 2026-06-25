1c920ec415c6d6c32611d3a0357f6c5b7b96b488
Rijal Rahman Zuhri <90142366+R2Z2407@users.noreply.github.com>
2026-06-25 14:33:50 +0700

    Refactor uploadImageToCloudinary function

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -1,20 +1,17 @@
 // src/lib/cloudinary.js
 
 export async function uploadImageToCloudinary(file) {
-  const cloudName = import.meta.env.PUBLIC_CLOUDINARY_CLOUD_NAME;
-  const uploadPreset = import.meta.env.PUBLIC_CLOUDINARY_UPLOAD_PRESET;
-
   if (!cloudName || !uploadPreset) {
     throw new Error("Cloudinary Cloud Name atau Upload Preset belum disetting di .env!");
   }
-
+  const uploadPreset = "benefit_karyawan";
   const formData = new FormData();
   formData.append("file", file);
   formData.append("upload_preset", uploadPreset);
 
   try {
     // Tembak langsung ke API Cloudinary
-    const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
+    const response = await fetch(`https://api.cloudinary.com/v1_1/dmqgtix3n/image/upload`, {
       method: "POST",
       body: formData,
     });
